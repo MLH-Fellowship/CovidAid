c4e1374d8a40cedd54d6c56e9b946fad0ad3efe8
Nitish Gadangi <nitishgadangi@gmail.com>
2020-10-12 17:33:58 +0530

    [App] double tap back to exit

--- App/app/src/main/java/com/bit_loons/covidaid/Activities/HomeActivity.java
+++ App/app/src/main/java/com/bit_loons/covidaid/Activities/HomeActivity.java
@@ -2,17 +2,38 @@ package com.bit_loons.covidaid.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bit_loons.covidaid.HistoryActivity;
 import com.bit_loons.covidaid.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding binding;
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,7 +63,7 @@ public class HomeActivity extends AppCompatActivity {
         binding.btnHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(HomeActivity.this, "UnderConstruction", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(HomeActivity.this, HistoryActivity.class));
             }
         });
 
